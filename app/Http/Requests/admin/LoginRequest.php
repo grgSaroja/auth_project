586c212dea8cedad16eafa3e1b4541c161b257f5
Halooween <saroja.grg33@gmail.com>
2022-11-01 15:28:12 +0545

    changes

--- app/Http/Requests/admin/LoginRequest.php
+++ app/Http/Requests/admin/LoginRequest.php
@@ -35,47 +35,5 @@ class LoginRequest extends FormRequest
         ];
     }
 
-    public function admin_authenticate()
-    {
-        $this->ensureIsNotRateLimited();
-
-        if (! Auth::guard('admin')->attempt($this->only('email', 'password'), $this->boolean('remember'))) {
-            RateLimiter::hit($this->throttleKey());
-
-            throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
-            ]);
-        }
-
-        RateLimiter::clear($this->throttleKey());
-    }
-
-    
-    public function ensureIsNotRateLimited()
-    {
-        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
-            return;
-        }
-
-        event(new Lockout($this));
-
-        $seconds = RateLimiter::availableIn($this->throttleKey());
-
-        throw ValidationException::withMessages([
-            'email' => trans('auth.throttle', [
-                'seconds' => $seconds,
-                'minutes' => ceil($seconds / 60),
-            ]),
-        ]);
-    }
-
-    /**
-     * Get the rate limiting throttle key for the request.
-     *
-     * @return string
-     */
-    public function throttleKey()
-    {
-        return Str::transliterate(Str::lower($this->input('email')).'|'.$this->ip());
-    }
+ 
  }
